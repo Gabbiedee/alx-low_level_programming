3ed0625593ebf37b4f2051c7ff0fe6565a8455f2
Gabriella <akingbojudolly@gmail.com>
2022-09-19 13:23:02 -0700

    arrays and pointers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - prints string
+ * @str: input string to print
+ * Description: prints string to stdout
+ *
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	int dol = 0;
+
+	while (*s != '\0')
+	{
+
+		dol++;
+		s++;
+	}
+
+	return (dol);
+}
